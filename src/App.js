4a2557437ee64cb8d47e4524f205e9a8f152ac2b
Elder Kampbell <elderkampbell@hotmail.com>
2022-08-18 17:57:56 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import PlanetCard from './components/PlanetCard';
+import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <>
       <Header />
       <SolarSystem />
-      <PlanetCard />
+      <Missions />
     </>
   );
 }
