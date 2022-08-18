334d9b664a19350a163b8ca61c2d59ad0e7da0c8
Elder Kampbell <elderkampbell@hotmail.com>
2022-08-18 16:00:56 -0300

    Requisitos 1, 2 e 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-import React from "react";
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
+}
